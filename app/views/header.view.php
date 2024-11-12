d982790cf5e1288584b53b64b435108cd8697f52
Nuran <nuranalwis@gmail.com>
2024-11-12 23:19:36 +0530

    doctor UI(not final)

--- app/views/header.view.php
+++ app/views/header.view.php
@@ -1,21 +1,29 @@
-<!-- Header -->
-<nav class="navbar">
-   <div class="navdiv">
-      <div class="logo-div">
-         <a href="../views/home.view.php" class="logo">
-            <img src="<?php echo ROOT ?>/assets/img/E_Care_logo.svg" alt="E-care logo">
-         </a>
-      </div>
-      <ul class="navbar-headings">
-         <li><a href="#">Home</a></li>
-         <li><a href="#">Services</a></li>
-         <li><a href="../Pages/Appointment.php">Appointment</a></li>
-         <li><a href="#">About</a></li>
-         <li><a href="#">Contact Us</a></li>
-      </ul>
-      <div class="buttons-div">
-         <button class="signin-btn"><a href="../Pages/SignIn.php">Sign In</a></button>
-         <button class="reg-btn"><a href="../Pages/SignIn.php">Register</a></button>
-      </div>
-   </div>
-</nav>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <link rel="stylesheet" type="text/css" href="<?php echo ROOT?>/assets/css/header.css">
+   </head>
+   <body>
+      <!-- Header -->
+      <nav class="navbar">
+         <div class="navdiv">
+            <div class="logo-div">
+               <a href="../views/home.view.php" class="logo">
+                  <img src="<?php echo ROOT ?>/assets/img/E_Care_logo.svg" alt="E-care logo">
+               </a>
+            </div>
+            <ul class="navbar-headings">
+               <li><a href="#">Home</a></li>
+               <li><a href="#">Services</a></li>
+               <li><a href="../Pages/Appointment.php">Appointment</a></li>
+               <li><a href="#">About</a></li>
+               <li><a href="#">Contact Us</a></li>
+            </ul>
+            <div class="buttons-div">
+               <button class="signin-btn"><a href="../Pages/SignIn.php">Sign In</a></button>
+               <button class="reg-btn"><a href="../Pages/SignIn.php">Register</a></button>
+            </div>
+         </div>
+      </nav>
+   </body>
+</html>

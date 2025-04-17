82c881b737b10be725893e2a33e04941417a8a3f
Okadini <okadini2002@gmail.com>
2025-04-17 12:39:14 +0530

    Made some syntax changes and made changes to the directory path

--- app/views/Patient/private_files.view.php
+++ app/views/Patient/private_files.view.php
@@ -50,15 +50,16 @@
 
                                 <div class="button-group">
                                     
-                                    <button class="private-view-button"><a href="<?= ROOT; ?>/assets/documents/<?php echo htmlspecialchars($document['document_name']) ?>" target="_blank">View</a></button>
+                                    <button class="private-view-button"><a href="<?= ROOT; ?>/assets/documents/<?= htmlspecialchars($_SESSION['USER']->user_id)?>/private_files/<?= htmlspecialchars($document['document_name']) ?>" target="_blank">View</a></button>
                                         
                                     <button class="private-edit-button" data-index="<?= $popupIndex ?>">Edit</button>
                                     <div class="popup" id="popup-<?= $popupIndex ?>" style="display: none;">
                                         <div class="popup-content">
                                             <form method="POST" action="<?= ROOT; ?>/patient/private_files">
-                                                <input type="hidden" name="document_id" value="<?php echo htmlspecialchars($document['document_id']) ?>">
+                                                <input type="hidden" name="user_id" value="<?= htmlspecialchars($_SESSION['USER']->user_id)?>">
+                                                <input type="hidden" name="document_id" value="<?= htmlspecialchars($document['document_id']) ?>">
                                                 <label for="document_name">Update Document Name:</label><br>
-                                                <input type="text" name="document_name" class="document-name" value="<?php echo htmlspecialchars($document['document_name']) ?>">
+                                                <input type="text" name="document_name" class="document-name" value="<?= htmlspecialchars($document['document_name']) ?>">
                                                 <button type="submit" name="update" class="update">Update</button>
                                             </form>
                                         </div>
@@ -67,8 +68,9 @@
                                     <?php $popupIndex++; ?>
                                     
                                     <form method="POST" onsubmit="return confirmDelete();">
-                                        <input type="hidden" name="document_id" value="<?php echo htmlspecialchars($document['document_id']) ?>">
-                                        <input type="hidden" name="document_name" value="<?php echo htmlspecialchars($document['document_name']) ?>">
+                                        <input type="hidden" name="user_id" value="<?= htmlspecialchars($_SESSION['USER']->user_id)?>">
+                                        <input type="hidden" name="document_id" value="<?= htmlspecialchars($document['document_id']) ?>">
+                                        <input type="hidden" name="document_name" value="<?= htmlspecialchars($document['document_name']) ?>">
                                         <button type="submit" name="delete" class="private-delete-button">Delete</button>
                                     </form>
                                 </div>
